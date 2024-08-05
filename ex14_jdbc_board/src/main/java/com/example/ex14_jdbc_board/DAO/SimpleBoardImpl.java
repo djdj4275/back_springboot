e7061f610ee2cff21959708070c8c99ee237994c
djdj4275 <djdj4275@naver.com>
2024-08-05 14:32:28 +0900

    08.02 (금) JDBC로 게시판 만들기 종료

--- ex14_jdbc_board/src/main/java/com/example/ex14_jdbc_board/DAO/SimpleBoardImpl.java
+++ ex14_jdbc_board/src/main/java/com/example/ex14_jdbc_board/DAO/SimpleBoardImpl.java
@@ -1,5 +1,58 @@
 package com.example.ex14_jdbc_board.DAO;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.ex14_jdbc_board.DTO.SimpleBoardDTO;
+
+import java.util.List;
+
+// 여기서 SimpleBoardDAO 참조하여 구현
+@Repository
 public class SimpleBoardImpl implements SimpleBoardDAO {
   
+  @Autowired
+  JdbcTemplate template;
+
+  @Override
+  public List<SimpleBoardDTO> listDAO() {
+    System.out.println("listDAO()");
+
+    String query = "select * from simple_board order by id asc";
+
+    List<SimpleBoardDTO> list = template.query(query, new BeanPropertyRowMapper<SimpleBoardDTO>(SimpleBoardDTO.class));
+
+    return list;
+  }
+
+  @Override
+  public SimpleBoardDTO viewDAO(String id) {
+    System.out.println("viewDAO()");
+
+    String query = "select * from simple_board where id = " + id;
+
+    SimpleBoardDTO dto = template.queryForObject(query, new BeanPropertyRowMapper<SimpleBoardDTO>(SimpleBoardDTO.class));
+
+    return dto;
+  }
+
+  @Override
+  public int writeDAO(String writer, String title, String content) {
+    System.out.println("writeDAO()");
+
+    String query = "insert into simple_board (writer, title, content) values (?, ?, ?)";
+
+    return template.update(query, writer, title, content);
+  }
+
+  @Override
+  public int deleteDAO(String id) {
+    System.out.println("deleteDAO()");
+
+    String query = "delete from simple_board where id = ?";
+
+    return template.update(query, Integer.parseInt(id)); // 문자열로 받은 id값을 정수형으로 변환후 삭제 업데이트
+  }
 }
--- ex14_jdbc_board/src/main/java/com/example/ex14_jdbc_board/MyController.java
+++ ex14_jdbc_board/src/main/java/com/example/ex14_jdbc_board/MyController.java
@@ -1,11 +1,19 @@
 package com.example.ex14_jdbc_board;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.ex14_jdbc_board.DAO.SimpleBoardDAO;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class MyController {
+
+  @Autowired
+  SimpleBoardDAO dao;
   
   // default 경로로 접근시 "/list"에 맞는곳으로 리다이렉트시킴
   @RequestMapping("/")
@@ -15,6 +23,31 @@ public class MyController {
 
   @RequestMapping("/list")
   public String userlistPage(Model model) {
+    model.addAttribute("list", dao.listDAO());
     return "list";
+  }
+
+  @RequestMapping("/view")
+  public String view(HttpServletRequest request, Model model) {
+    String id = request.getParameter("id");
+    model.addAttribute("view", dao.viewDAO(id));
+    return "view";
+  }
+
+  @RequestMapping("/write-form")
+  public String writeForm() {
+    return "write-form";
+  }
+
+  @RequestMapping("/write")
+  public String write(Model model, HttpServletRequest request) {
+    dao.writeDAO(request.getParameter("writer"), request.getParameter("title"), request.getParameter("content"));
+    return "redirect:list";
+  }
+
+  @RequestMapping("/delete")
+  public String delete(Model model, HttpServletRequest request) {
+    dao.deleteDAO(request.getParameter("id"));
+    return "redirect:list";
   }
 }
